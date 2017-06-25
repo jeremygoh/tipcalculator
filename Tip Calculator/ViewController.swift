349eb242937afdc11f40adb18b40e2c50051a42d
Jeremy <jg485dev@gmail.com>
2017-06-24 23:21:14 -0700

    ui tweaks and animation

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -15,14 +15,19 @@ struct Constants {
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var billContainerView: UIView!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    var billFieldModified = false
+    
+    @IBOutlet weak var plusLabel: UILabel!
     override func viewWillAppear(_ animated: Bool) {
         let defaults = UserDefaults.standard
         tipControl.selectedSegmentIndex = defaults.integer(forKey: Constants.defaultTipIndexKey)
+        billField.becomeFirstResponder()
     }
     
     override func viewDidLoad() {
@@ -36,12 +41,32 @@ class ViewController: UIViewController {
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true);
     }
+    
 
     @IBAction func tipPercentChanged(_ sender: Any) {
-        calculateTip(sender: sender)
+        calculateTip()
     }
 
-    @IBAction func calculateTip(_ sender: Any) {
+    @IBAction func billTextChanged(_ sender: Any) {
+        calculateTip()
+        if !billFieldModified {
+            let navigationBar = self.navigationController!.navigationBar
+            let verticalHeightAvailable = view.frame.height - navigationBar.frame.height
+            UIView.animate(withDuration: 0.2, animations: {
+                self.billContainerView.frame = CGRect(x: 0, y: navigationBar.frame.origin.y + navigationBar.frame.size.height, width: self.view.frame.width, height: verticalHeightAvailable * 0.35)
+                self.billField.frame = CGRect(x: 0, y: self.billContainerView.frame.origin.y, width: self.billField.frame.width, height: verticalHeightAvailable * 0.1)
+                self.tipControl.frame = CGRect(x: self.tipControl.frame.origin.x, y: self.billField.frame.origin.y + self.billField.frame.height + 20, width: self.tipControl.frame.width, height: self.tipControl.frame.height)
+                self.totalLabel.isHidden = false
+                self.tipControl.isHidden = false
+                self.tipLabel.isHidden = false
+                self.plusLabel.isHidden = false
+            })
+            
+        }
+        billFieldModified = true
+    }
+    
+    private func calculateTip() {
         let bill = Double(billField.text!) ?? 0
         let tip = bill * Constants.tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
